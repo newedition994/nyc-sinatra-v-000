f5068c3e86f1b81ef8213adb86d067a4a1542d9a
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-08 23:50:09 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,27 +9,13 @@ class FiguresController < ApplicationController
  +    erb :'/figures/new'
  +  end
  +
- +  post '/figures' do
- +    @figure = Figure.create(name: params["figure"]["name"])
- +
- +    #if !params["figure"]["title_ids"].empty?
- +    #  @figure.titles << params["figure"]["title_ids"].collect do |title_id|
- +    #    Title.find_by(id: title_id)
- +    #  end
- +    #end
- +
- +    @figure.title_ids = params["figure"]["title_ids"]
- +
- +    if !params["title"]["name"].empty?
- +      @figure.titles << Title.find_or_create_by(name: params["title"]["name"])
- +    end
- +
- +    #if !params["figure"]["landmark_ids"].empty?
- +      #@figure.landmarks << #params["figure"]["landmark_ids"].collect do |landmark_id|
- +    #    Landmark.find_by(id: landmark_id)
- +    #  end
- +    #end
- +
+   post '/figures' do
+     @figure = Figure.create(name: params["figure"]["name"])
+ 
+     @figure.title_ids = params["figure"]["title_ids"]
+     if !params["title"]["name"].empty?
+       @figure.titles << Title.find_or_create_by(name: params["title"]["name"])
+     end
      @figure.landmark_ids = params["figure"]["landmark_ids"]
      if !params["landmark"]["name"].empty?
        @figure.landmarks << Landmark.find_or_create_by(name: params["landmark"]["name"])
