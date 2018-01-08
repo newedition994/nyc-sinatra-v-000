79a5ce18b347e75daed019ed110da82434e5b892
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-08 23:51:24 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -37,11 +37,9 @@ class FiguresController < ApplicationController
    post '/figures/:id' do
      @figure = Figure.find(params[:id])
      @figure.update(params["figure"])
-
      if !params["title"]["name"].empty?
        @figure.titles << Title.find_or_create_by(name: params["title"]["name"])
      end
-
      if !params["landmark"]["name"].empty?
        @figure.landmarks << Landmark.find_or_create_by(name: params["landmark"]["name"])
      end
