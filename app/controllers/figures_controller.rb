c752d93cdc6a094ee29ef9fed42cc576a9b1f93e
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-08 23:50:32 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,17 +1,17 @@
 class FiguresController < ApplicationController
- +
- +  get '/figures' do
- +    @figures = Figure.all
- +    erb :'/figures/index'
- +  end
- +
- +  get '/figures/new' do
- +    erb :'/figures/new'
- +  end
- +
+
+   get '/figures' do
+     @figures = Figure.all
+     erb :'/figures/index'
+   end
+
+   get '/figures/new' do
+     erb :'/figures/new'
+   end
+
    post '/figures' do
      @figure = Figure.create(name: params["figure"]["name"])
- 
+
      @figure.title_ids = params["figure"]["title_ids"]
      if !params["title"]["name"].empty?
        @figure.titles << Title.find_or_create_by(name: params["title"]["name"])
