ccda691aad292b0707dc28489d8bf29905c49684
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-08 23:36:53 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -33,16 +33,20 @@ class FiguresController < ApplicationController
    end
 
    post '/figures/:id' do
-     @figure = Figure.find_by_id(params[:id])
-     @figure.update(params["figure"])
-     if !params[:landmark][:name].empty?
-       @figure.landmarks << Landmark.create(params[:landmark])
-     end
-     if !params[:title][:name].empty?
-       @figure.titles << Title.create(params[:title])
-     end
-     @figure.save
-     redirect to "/figures/#{@figure.id}"
+     @figure = Figure.find(params[:id])
+ +    @figure.name = params["figure"]["name"]
+ +    if !Title.find_by_id(params["figure"]["title_ids"])
+ +      @figure.titles << Title.create(name: params["title"]["name"])
+ +    else
+ +      @figure.titles << Title.find_by_id(params["figure"]["title_ids"])
+ +    end
+ +    if !Landmark.find_by_id(params["figure"]["landmark_ids"])
+ +      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+ +    else
+ +      @figure.landmarks << Landmark.find_by_id(params["figure"]["landmark_ids"])
+ +    end
+ +    @figure.save
+ +    redirect to "/figures/#{@figure.id}"
    end
 
 end
