5e722ab5c259af8b73c954f7b206cca5cccf8741
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-08 23:37:26 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -34,19 +34,19 @@ class FiguresController < ApplicationController
 
    post '/figures/:id' do
      @figure = Figure.find(params[:id])
- +    @figure.name = params["figure"]["name"]
- +    if !Title.find_by_id(params["figure"]["title_ids"])
- +      @figure.titles << Title.create(name: params["title"]["name"])
- +    else
- +      @figure.titles << Title.find_by_id(params["figure"]["title_ids"])
- +    end
- +    if !Landmark.find_by_id(params["figure"]["landmark_ids"])
- +      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
- +    else
- +      @figure.landmarks << Landmark.find_by_id(params["figure"]["landmark_ids"])
- +    end
- +    @figure.save
- +    redirect to "/figures/#{@figure.id}"
+     @figure.name = params["figure"]["name"]
+     if !Title.find_by_id(params["figure"]["title_ids"])
+       @figure.titles << Title.create(name: params["title"]["name"])
+     else
+       @figure.titles << Title.find_by_id(params["figure"]["title_ids"])
+     end
+     if !Landmark.find_by_id(params["figure"]["landmark_ids"])
+       @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+     else
+       @figure.landmarks << Landmark.find_by_id(params["figure"]["landmark_ids"])
+     end
+     @figure.save
+     redirect to "/figures/#{@figure.id}"
    end
 
 end
