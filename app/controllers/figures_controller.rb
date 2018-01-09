baf275dfd400c2d08a5dc9b59b0072edd5306b4c
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-09 00:42:24 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,49 +1,48 @@
+require 'pry'
+
 class FiguresController < ApplicationController
+
    get '/figures' do
      @figures = Figure.all
-     erb :"figures/index"
+     erb :'/figures/index'
    end
 
    get '/figures/new' do
-     erb :"figures/new"
+     erb :'/figures/new'
    end
 
    post '/figures' do
-     @figure = Figure.create(params["figure"])
-     if !params[:landmark][:name].empty?
-       @figure.landmarks << Landmark.create(params[:landmark])
-     end
+     @figure = Figure.create(name: params["figure"]["name"])
 
-     if !params[:title][:name].empty?
-       @figure.titles << Title.create(params[:title])
+     @figure.title_ids = params["figure"]["title_ids"]
+     if !params["title"]["name"].empty?
+       @figure.titles << Title.find_or_create_by(name: params["title"]["name"])
      end
-
+     @figure.landmark_ids = params["figure"]["landmark_ids"]
+     if !params["landmark"]["name"].empty?
+       @figure.landmarks << Landmark.find_or_create_by(name: params["landmark"]["name"])
+     end
      @figure.save
-     redirect to "/figures/#{@figure.id}"
-   end
-
-   get '/figures/:id/edit' do
-     @figure = Figure.find_by_id(params[:id])
-     erb :"figures/edit"
+     redirect "/figures/#{@figure.id}"
    end
 
    get '/figures/:id' do
      @figure = Figure.find_by_id(params[:id])
-     binding.pry
-     erb :"figures/show"
+     erb :'/figures/show'
+   end
+
+   get '/figures/:id/edit' do
+     @figure = Figure.find_by_id(params[:id])
+     erb :'/figures/edit'
    end
 
    post '/figures/:id' do
-     @figure = Figure.find_by_id(params[:id])
-     @figure.update(params["figure"])
-     if !params[:landmark][:name].empty?
-       @figure.landmarks << Landmark.create(params[:landmark])
-     end
-     if !params[:title][:name].empty?
-       @figure.titles << Title.create(params[:title])
-     end
+     @figure = Figure.find(params[:id])
+     @figure.update(params[:figure])
+     @figure.titles << Title.find_or_create_by(name: params["title"]["name"])
+     @figure.landmarks << Landmark.find_or_create_by(name: params["landmarks"]["name"])
      @figure.save
-     redirect to "/figures/#{@figure.id}"
+     redirect "/figures/#{@figure.id}"
    end
 
 end
